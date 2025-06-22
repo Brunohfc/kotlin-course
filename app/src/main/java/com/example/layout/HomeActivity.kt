480b31990c08cec4fc7cf6210c3368aa93a26473
Bruno Henrick <91575338+Brunohfc@users.noreply.github.com>
2025-06-22 14:42:49 -0300

    criando intent para navegacao e validacao simples

--- app/src/main/java/com/example/layout/HomeActivity.kt
+++ app/src/main/java/com/example/layout/HomeActivity.kt
@@ -1,38 +1,20 @@
 package com.example.layout
 
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.layout.databinding.ActivityLoginBinding
-import com.example.layout.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
-
-    private lateinit var binding: ActivityLoginBinding
-
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-        binding = ActivityLoginBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
+        setContentView(R.layout.activity_home)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        binding.buttonLogin.setOnClickListener(this)
-        binding.buttonRegister.setOnClickListener(this)
     }
-
-    override fun onClick(p0: View?) {
-        TODO("Not yet implemented")
-    }
-
-
 }
--- app/src/main/java/com/example/layout/loginActivity.kt
+++ app/src/main/java/com/example/layout/loginActivity.kt
@@ -1,20 +1,54 @@
 package com.example.layout
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.method.Touch
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.layout.databinding.ActivityLoginBinding
+import android.util.Log;
+import android.widget.Toast
 
-class loginActivity : AppCompatActivity() {
+class loginActivity : AppCompatActivity() , View.OnClickListener{
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.buttonLogin.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+
+        //criando a intenção de navegando
+        val intent: Intent = Intent(this, HomeActivity::class.java)
+
+        if(v?.id == R.id.button_login){
+            var email = binding.editTextEmail.text.toString()
+            var password = binding.editTextPassword.text.toString()
+            Log.d("LoginActivity", "Email: $email senha: $password")
+
+            if(email.isEmpty() || password.isEmpty()){
+                Toast.makeText(this,R.string.campos_vazios, Toast.LENGTH_SHORT).show()
+            }else{
+                startActivity(intent)
+            }
+
         }
     }
 }
